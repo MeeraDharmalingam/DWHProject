87940b1ab6c091dd3b3b44c5954cf2e23679128f
MeeraDharmalingam <76567712+MeeraDharmalingam@users.noreply.github.com>
2022-01-23 18:08:03 -0500

    Add files via upload

---
+++ etl (1).py
@@ -0,0 +1,61 @@
+import configparser
+import psycopg2
+import pandas as pd
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        print(query)
+        cur.execute(query)
+        conn.commit()
+        print('execution done')
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+    KEY                    = config.get('AWS','KEY')
+    SECRET                 = config.get('AWS','SECRET')
+    AWS_REGION             = config.get('AWS',"REGION")
+
+    df = pd.DataFrame({"Param": ["DWH_Key" , "DWH_Secret"], "Value": [KEY, SECRET]})
+    print(df)
+
+    DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
+    DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
+    DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
+
+    DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+    DWH_DB                 = config.get("DWH","DWH_DB")
+    DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
+    DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
+    DWH_PORT               = config.get("DWH","DWH_PORT")
+
+    DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+
+    df = pd.DataFrame({"Param":
+                  ["DWH_CLUSTER_TYPE", "DWH_NUM_NODES", "DWH_NODE_TYPE", "DWH_CLUSTER_IDENTIFIER", "DWH_DB", "DWH_DB_USER",    "DWH_DB_PASSWORD", "DWH_PORT", "DWH_IAM_ROLE_NAME"],
+              "Value":
+                  [DWH_CLUSTER_TYPE, DWH_NUM_NODES, DWH_NODE_TYPE, DWH_CLUSTER_IDENTIFIER, DWH_DB, DWH_DB_USER,      DWH_DB_PASSWORD, DWH_PORT, DWH_IAM_ROLE_NAME]
+             })
+    print(df)
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)   
+    conn.commit()    
+    
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
